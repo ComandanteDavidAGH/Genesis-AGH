641082c0c6550e99d5a3addcc8ed4155d48891fe
ComandanteDavidAGH <jorge.dpacheco1980@gmail.com>
2026-05-01 09:04:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -464,7 +464,10 @@ elif menu == "📸 Eventos Institucionales":
    st.markdown("<h3 style='color:#000000; border-bottom:3px solid #d4af37; padding-bottom:5px; font-family:Arial Black;'>📸 Memorias Institucionales</h3>", unsafe_allow_html=True)
    c1, c2, c3 = st.columns(3)
    c1.image("https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=800", caption="Feria Científica")
-                                                                                                                                                                                                                                             # --- PIE DE PÁGINA LEGAL (Habeas Data) ---
+   c2.image("https://images.unsplash.com/photo-1523580494112-071d1694335c?q=80&w=800", caption="Ceremonia de Grados")
+   c3.image("https://images.unsplash.com/photo-1511632765486-a01980e01a18?q=80&w=800", caption="Comunidad Estudiantil")
+
+# --- PIE DE PÁGINA LEGAL (Habeas Data) ---
 st.markdown(f"""
     <div class='footer-legal'>
         Academia Global Horizonte - Sistema Génesis AGH © {datetime.now().year}<br>
@@ -472,5 +475,3 @@ st.markdown(f"""
         el tratamiento de la información aquí registrada es de carácter estrictamente institucional y confidencial.
     </div>
 """, unsafe_allow_html=True)
-   c2.image("https://images.unsplash.com/photo-1523580494112-071d1694335c?q=80&w=800", caption="Ceremonia de Grados")
-   c3.image("https://images.unsplash.com/photo-1511632765486-a01980e01a18?q=80&w=800", caption="Comunidad Estudiantil")
