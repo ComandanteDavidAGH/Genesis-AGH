bf2aed7c08b4c48ea1ff3143381619d1b54b499f
ComandanteDavidAGH <jorge.dpacheco1980@gmail.com>
2026-05-02 15:09:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -580,6 +580,7 @@ elif menu == "📜 Boletines":
                 # --- 2. CICLO DE NOTAS Y LOGROS ---
                 for _, row in res.iterrows():
                     td = f"<td>{row['P1']:.1f}</td><td>{row['P2']:.1f}</td><td>{row['P3']:.1f}</td><td>{row['P4']:.1f}</td><td><b style='color:#0d1b2a;'>{row['PROMEDIO']:.1f}</b></td>" if periodo_sel == "CONSOLIDADO FINAL" else f"<td>{row[periodo_sel]:.1f}</td>"
+                    st.write("Columnas detectadas:", list(row.index))
                     html_boletin += f"<tr><td style='text-align:left;'>{row['Materia']}</td>{td}<td style='font-weight:bold;'>{row['DESEMPEÑO']}</td></tr>"
                     
                     col_span = 7 if periodo_sel == "CONSOLIDADO FINAL" else 3
